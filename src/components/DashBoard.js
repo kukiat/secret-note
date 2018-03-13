57c513af54b29a994c370b628a3b80b22dffaa84
kukiat <kukiat1996@gmail.com>
2018-03-13 20:10:11 +0700

    check

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -26,6 +26,7 @@ class DashBoard extends React.Component {
     const ref = await db.ref('note').once('value')
     const notes = ref.val()
     const newNotes = []
+    //have user
     Object.keys(notes).map((key) => {
       if(this.props.userId === notes[key].userId) {
         newNotes.push(Object.assign(notes[key], { id: key }))
@@ -47,33 +48,59 @@ class DashBoard extends React.Component {
 
   async componentDidUpdate(prevProps, prevState) {
     const { indexTitle, titles, value, tabId } = this.state
-    const checkTab = titles[indexTitle].content === value
-    if(prevState.value !== '' && value !== '' && !checkTab) {
+    let checkTab = false
+    console.log(value, titles, prevState.titles, indexTitle)
+    if(indexTitle !== titles.length)
+      checkTab = titles[indexTitle].content === value
+    else
+      checkTab = true
+    if(!this.isBlank(prevState.value) && !this.isBlank(value) && !checkTab) {
       await db.ref('note').child(tabId).update({
         content: value
       })
       Object.assign(titles[indexTitle], {content: value})
+      console.log('saved')
       this.setState({ titles: titles })
     }
+    if(this.isBlank(prevState.value) && prevState.tabId && tabId !== prevState.tabId) {
+      await db.ref('note').child(prevState.tabId).remove()
+      titles.splice(prevState.indexTitle, 1)
+      if(prevState.indexTitle > indexTitle)
+        indexTitle
+      else
+        indexTitle-1
+      console.log('removed')
+      this.setState({ 
+        titles: titles,
+        indexTitle: indexTitle
+      })
+    }
+  }
+  //if true==''
+  isBlank = (text) => {
+    return !text || text.length === 0 || /^\s*$/.test(text)
   }
 
   onContentChange = (index, id) => {
     const { titles } = this.state
-    const titleContent = titles[index].content
-    this.setState({ 
-      value : titleContent,
-      tabId: id,
-      indexTitle: index
-    })
+      console.log(titles[index].content)
+      const titleContent = titles[index].content
+      this.setState({ 
+        value : titleContent,
+        tabId: id,
+        indexTitle: index
+      })
+    
   }
 
   addTitle = async () => {
-    const newNote = {title: 'newTitle', userId: this.props.userId, content: ''}
+    const newNote = {title: 'newTitle', userId: this.props.userId, content: 'New Note'}
     const ref = await db.ref('note').push(newNote)
     this.state.titles.unshift(Object.assign(newNote, { id: ref.key}))
     this.setState({
       titles: this.state.titles,
-      value: ''
+      value: newNote.content,
+      indexTitle: 0
     })
   }
   
